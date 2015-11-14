94c838fd95e2dce6882c7ceb09d26df43ba68371
Colin McKenna <colinjmckenna@gmail.com>
2015-11-13 21:56:41 -0800

    add preferred settings

--- app/controllers/Welcome.py
+++ app/controllers/Welcome.py
@@ -1,11 +1,3 @@
-"""
-    Sample Controller File
-
-    A Controller should be in charge of responding to a request.
-    Load models to interact with the database and load views to render them to the client.
-
-    Create a controller using this template
-"""
 from system.core.controller import *
 
 class Welcome(Controller):
@@ -20,8 +12,8 @@ class Welcome(Controller):
 
     """ This is an example of a controller method that will load a view for the client """
     def index(self):
-        """ 
-        A loaded model is accessible through the models attribute 
+        """
+        A loaded model is accessible through the models attribute
         self.models['WelcomeModel'].get_all_users()
         """
         return self.load_view('index.html')
